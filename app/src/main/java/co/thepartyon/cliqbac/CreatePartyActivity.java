b188f98cc9124919050610a5ecc48ac3121a8447
aymswick <asymkowick@gmail.com>
2016-03-25 13:03:57 -0700

    Changed theme and added DatePickerDialog

--- app/src/main/java/co/thepartyon/cliqbac/CreatePartyActivity.java
+++ app/src/main/java/co/thepartyon/cliqbac/CreatePartyActivity.java
@@ -1,12 +1,17 @@
 package co.thepartyon.cliqbac;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import java.text.DateFormatSymbols;
 import java.util.Calendar;
 
 public class CreatePartyActivity extends AppCompatActivity implements View.OnClickListener{
@@ -18,8 +23,14 @@ public class CreatePartyActivity extends AppCompatActivity implements View.OnCli
 
 
         //Instantiate Buttons + Set onClick Listeners
-        final View startTime = (EditText) findViewById(R.id.startTimeEditText);
+        View startTime = (EditText) findViewById(R.id.startTimeEditText);
         startTime.setOnClickListener(this);
+
+        View startDate = (EditText) findViewById(R.id.startDateEditText);
+        startDate.setOnClickListener(this);
+
+        View confirm = (Button) findViewById(R.id.confirmButton);
+        confirm.setOnClickListener(this);
 
     }
 
@@ -41,7 +52,7 @@ public class CreatePartyActivity extends AppCompatActivity implements View.OnCli
                     } else {
                         AM_PM = "PM";
                     }
-                    startTime.setText( "" + (Math.abs(selectedHour - 12)) + ":" + selectedMinute + " " + AM_PM);
+                    startTime.setText((Math.abs(selectedHour - 12)) + ":" + selectedMinute + " " + AM_PM);
                 }
             }, hour, minute, false);
             mTimePicker.setTitle("Select Time");
@@ -49,7 +60,37 @@ public class CreatePartyActivity extends AppCompatActivity implements View.OnCli
 
         }
 
-        //else if(v.getId() == R.id.date)
+        else if(v.getId() == R.id.startDateEditText) //Click Date, bring up DatePickerDialog
+        {
+            final EditText startDate = (EditText) v;
+            Calendar mcurrentDate = Calendar.getInstance();
+            int day = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+            int month = mcurrentDate.get(Calendar.MONTH);
+            int year = mcurrentDate.get(Calendar.YEAR);
+
+            DatePickerDialog mDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+
+                    startDate.setText(getMonth(monthOfYear) + " " + dayOfMonth + ", " + year);
+                }
+            }, year, month, day);
+            mDatePicker.setTitle("Select Date");
+            mDatePicker.show();
+
+        }
+
+        else if(v.getId() == R.id.confirmButton) //User clicks the confirm button, data is stored in DB
+        {
+            //Store info in DB and move to next activity
+            Snackbar.make(v, "Confirmed.", Snackbar.LENGTH_SHORT).show();
+        }
 
     }
+
+    public String getMonth(int month) {
+        return new DateFormatSymbols().getMonths()[month-1];
+    }
+
 }
