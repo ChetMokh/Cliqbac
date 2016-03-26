cc150e9aaf7d54cdea5a377f3a09680364bda7c1
aymswick <asymkowick@gmail.com>
2016-03-25 21:40:09 -0700

    FRIDAY EOD STABLE BUILD

--- app/src/main/java/co/thepartyon/cliqbac/MapsActivity.java
+++ app/src/main/java/co/thepartyon/cliqbac/MapsActivity.java
@@ -48,13 +48,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.setMyLocationEnabled(true);
         }
 
+        else
+        {
+            //TODO: Request the User's Permission for location for devices running Marshmallow
+        }
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-
-
     }
 }
